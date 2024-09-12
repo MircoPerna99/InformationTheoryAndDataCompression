160192a1b4def031e9789ec7996fe0c1ca3f1003
MircoPerna99 <mircoperna99@gmail.com>
2024-09-12 17:00:33 +0200

    Create Entropy class

--- Entropy/Entropy.py
+++ Entropy/Entropy.py
@@ -1,1 +1,18 @@
-print('Try algorithm')
+import math
+
+class Entropy:    
+    def __init__(self, probabilities):
+        self.probabilities = probabilities
+        self.entropyValue = self.CalculateEntropy()
+        
+    def CalculateEntropy(self):
+        entropyCalculated = 0
+        for probability in  self.probabilities:
+            entropyCalculated = entropyCalculated + probability*math.log2(1/probability)
+    
+        return entropyCalculated
+        
+
+prova = Entropy([1/3,1/3,1/3])
+
+print(prova.entropyValue)
