71c5614f847c39fde9c1fc699869ec21987167ef
MircoPerna99 <mircoperna99@gmail.com>
2024-09-13 11:11:01 +0200

    AddCalculateFrequencyOfCharsOnAText

--- Entropy/Entropy.py
+++ Entropy/Entropy.py
@@ -1,18 +1,38 @@
 import math
 
 class Entropy:    
-    def __init__(self, probabilities):
-        self.probabilities = probabilities
-        self.entropyValue = self.CalculateEntropy()
-        
-    def CalculateEntropy(self):
+    def CalculateEntropy(self, probabilities):
         entropyCalculated = 0
-        for probability in  self.probabilities:
+        for probability in  probabilities:
             entropyCalculated = entropyCalculated + probability*math.log2(1/probability)
     
         return entropyCalculated
+    
+    def CalculateFrequencyOfCharsOnAText(self, nameFile):
+        dictionary = {}
+        file = open(nameFile, "r")
+        amountOfChars = 0
+        for line in file:
+            for char in line:
+                amountOfChars = amountOfChars+1
+                if not dictionary or not char in dictionary:
+                    dictionary[char] = 1
+                else:
+                    dictionary[char] = dictionary[char]+1
+        
+        for key in dictionary:
+            dictionary[key] = dictionary[key]/amountOfChars
+        
+        return dictionary
+    
+    def CalculateEntropyOfAText(self, nameFile):
+        dictionary = self.CalculateFrequencyOfCharsOnAText(nameFile)
+        print(dictionary)
+    
+
         
 
-prova = Entropy([1/3,1/3,1/3])
+prova = Entropy()
 
-print(prova.entropyValue)
+print(prova.CalculateEntropy([1/3,1/3,1/3]))
+prova.CalculateEntropyOfAText("Prova.txt")
